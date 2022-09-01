03cb31d4fecc73927184299669f17a567f80c125
ronyhe <ronyheruth@wix.com>
2022-09-01 20:05:32 +0300

    config

--- test/example.spec.mjs
+++ test/example.spec.mjs
@@ -1,6 +1,6 @@
-import test from 'ava'
-import {add} from "../src/example.mjs";
+import test from "ava";
+import { add } from "../src/example.mjs";
 
-test('add', t => {
-    t.deepEqual(add(2, 3), 5)
-})
+test("add", (t) => {
+  t.deepEqual(add(2, 3), 5);
+});
