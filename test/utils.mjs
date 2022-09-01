e35ff6b2cf0cc757962054f5b9376724bbbbe99a
ronyhe <ronyheruth@wix.com>
2022-09-01 21:22:01 +0300

    Accept strings in ast assertions

--- test/utils.mjs
+++ test/utils.mjs
@@ -1,7 +1,11 @@
 import { parse } from '@babel/parser'
 import { cloneDeepWithoutLoc } from '@babel/types'
 
+const isString = s => typeof s === 'string'
+
+const toAst = source => (isString(source) ? parse(source) : source)
+
 export function assertEqualAst(t, a, b) {
-    const process = source => cloneDeepWithoutLoc(parse(source))
+    const process = source => cloneDeepWithoutLoc(toAst(source))
     t.deepEqual(process(a), process(b))
 }
--- test/utils.spec.mjs
+++ test/utils.spec.mjs
@@ -1,8 +1,13 @@
 import test from 'ava'
 import { assertEqualAst } from './utils.mjs'
+import { parse } from '@babel/parser'
 
 test('identical asts are equal', t => {
     assertEqualAst(t, 'a = 1', 'a = 1')
+})
+
+test('accepts strings or asts', t => {
+    assertEqualAst(t, 'a = 1', parse('a = 1'))
 })
 
 test('locations are irrelevant', t => {
